d8ecfb75e3a4b42ef9b91c4598754980d1c77911
Jim Vang <vangjim15@gmail.com>
2017-04-18 13:33:55 -0500

    last work on the assignment

--- scripts.js
+++ scripts.js
@@ -17,9 +17,13 @@ function onReady() {
 // turning img to a click event
 function guessPicture() {
  console.log('testing guessPicture'); // checking if guessPicture is working
-name = $(this).data('name');
- console.log(name);
+ name = $(this).data('name');
  compareUserGuess();
+}
+
+//this generates a random number - used in appendName
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
 //we are appending names
@@ -29,16 +33,18 @@ function appendName() {
   $('.name-guess').text(randomPerson);//appending to the DOM
 }
 
-//this generates a random number - used in appendName
-function randomNumber(min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);
-}
-
 //this compares the guess to the correct answer
 function compareUserGuess() {
   if (name === randomPerson) {
-    $('.results').text("You got it dude!");
+    $('.results').text("You got it dude!").fadeIn('slow').fadeOut('slow');
+    // onReady();
+    guessAgain();
   } else {
-    $('.results').text("Wrong!!! Try again");
+    $('.results').text("Wrong!!! Try again").fadeIn('slow').fadeOut('slow');
   }
+}
+
+function guessAgain() {
+  console.log('testing guessAgain');
+  appendName();
 }
